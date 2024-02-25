5dafa528fc714b64760375584c7aa0b0d81d4784
Noah William Shaffer <noahwilliamshaffer@gmail.com>
2024-02-24 18:43:37 -0800

    Can change colors and display text

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from 'react';
 
 export default function App() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <h1 className='text-red-500'> App </h1>; 
 }
