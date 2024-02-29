e8de5ccc2263a910506d2613b5d46f41820677b2
Noah William Shaffer <noahwilliamshaffer@gmail.com>
2024-02-29 11:16:13 -0800

    Added a seacrh bar to the header.

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -7,6 +7,10 @@ function Header() {
             <span className = 'text-slate-500'> Shaffer </span>
             <span className = 'text-slate-700'> Estate </span>    
         </h1>
+        <form>
+            <input type = "Text" Placeholder = "Search..." />
+        </form>
+        
     </header>
   )
 }
