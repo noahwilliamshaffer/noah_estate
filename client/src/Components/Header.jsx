5475c7aea851dce20aefa36e09b2c8b7447a61bc
Noah William Shaffer <noahwilliamshaffer@gmail.com>
2024-02-29 10:40:44 -0800

    Created my header.

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>
-      Header
-    </div>
+    <header>
+        <h1 className = ' font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className = 'text-slate-500'> Shaffer </span>
+            <span className = 'text-slate-700'> Estate </span>    
+        </h1>
+    </header>
   )
 }
 
