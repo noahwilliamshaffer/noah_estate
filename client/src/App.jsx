ab07eb44210f3d492b0f5f832047ea523618c980
Noah William Shaffer <noahwilliamshaffer@gmail.com>
2024-02-29 10:22:20 -0800

    Header added to every page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,14 @@ import Profile from './pages/Profile';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import SignIn from './pages/SignIn';
+import Header from './components/Header';
 
 
 
 
 export default function App() {
   return <BrowserRouter>
+  <Header/>
   <Routes>
     <Route path="/" element = {<Home/>} />
     <Route path="/Profile" element = {<Profile/>} />
