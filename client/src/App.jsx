05688942e0f9da9a18116d825ad1d868cf9ac2dc
Noah William Shaffer <noahwilliamshaffer@gmail.com>
2024-02-29 09:57:22 -0800

    About Works

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import SignUp from './pages/SignUp';
+import About from './pages/About';
 
 
 
@@ -12,6 +13,7 @@ export default function App() {
     <Route path="/" element = {<Home/>} />
     <Route path="/Profile" element = {<Profile/>} />
     <Route path="/SignUp" element = {<SignUp/>} />
+    <Route path="/About" element = {<About/>} />
 
   </Routes>
   
