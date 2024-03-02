0d0968edd73541a85160b06289d95517551d0e81
Noah William Shaffer <noahwilliamshaffer@gmail.com>
2024-03-01 19:17:24 -0800

    Fixed the width of the search bar and my title bar.

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -2,22 +2,19 @@ import {FaSearch } from 'react-icons/fa';
 
 function Header() {
   return (
-    <header className = 'bg-slate-300 shadow-md' >
-        <div className = 'flex justify-between items-center max-w-6xl mx-auto p-3'>
-        <h1 className = ' font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className = 'text-slate-500'> Shaffer </span>
-            <span className = 'text-slate-700'> Estate </span>    
+    <header className='bg-slate-300 shadow-md'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+          <span className='text-slate-500'>Shaffer</span>
+          <span className='text-slate-700'>Estate</span>
         </h1>
-        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-            <input type = "Text" Placeholder = "Search..." className = 'bg-transparent focus:outline-none w-24 sm:w-64' />
-        <FaSearch className='text-slate-600'/>
+        <form className='bg-slate-100 rounded-lg flex items-center'>
+          <input type="text" placeholder="Search..." className='bg-transparent focus:outline-none w-full px-4 py-1 sm:py-2' />
+          <FaSearch className='text-slate-600 text-lg sm:text-2xl p-2'/>
         </form>
-
-        </div>
-
-        
+      </div>
     </header>
-  )
+  );
 }
 
 export default Header
