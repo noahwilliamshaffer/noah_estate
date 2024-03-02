574a0962f18ed8750c7cfdd953a1b252fe1b588a
Noah William Shaffer <noahwilliamshaffer@gmail.com>
2024-03-01 19:02:55 -0800

    Made the background to my header slightly darker.

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -2,7 +2,7 @@ import {FaSearch } from 'react-icons/fa';
 
 function Header() {
   return (
-    <header className = 'bg-slate-200 shadow-md' >
+    <header className = 'bg-slate-300 shadow-md' >
         <div className = 'flex justify-between items-center max-w-6xl mx-auto p-3'>
         <h1 className = ' font-bold text-sm sm:text-xl flex flex-wrap'>
             <span className = 'text-slate-500'> Shaffer </span>
