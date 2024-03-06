85de6121817125a78c85f8650a4b87886b9c361a
Noah William Shaffer <noahwilliamshaffer@gmail.com>
2024-03-05 20:58:10 -0500

    Connected to mongo db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-mongoose.connect("mongodb+srv://Shaffer:Bulqy2020@shaffer-estate.6aprdww.mongodb.net/shaffer=estate?retryWrites=true&w=majority&appName=shaffer-estate")
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log('Connected to MongoDB!');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 const app = express();
 
