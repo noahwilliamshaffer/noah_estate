742cffb0c889608448ee3c25fb7c60c51addb2a6
Noah William Shaffer <noahwilliamshaffer@gmail.com>
2024-03-18 15:00:28 -0400

    It works and mongo db contains username and password but we will hash the password in future.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,11 @@
 import User from "../models/user.model.js";
 
-export const signup = (req, res) => {
+export const signup = async (req, res) => {
     //info we get from browser
    const {username, email, password} = req.body;
-   const newUser = new Userer({username, email, password});
+   const newUser = new User({username, email, password});
+   //this saving takes time so we use await and change function to async
+   await newUser.save()
+   //201 is for created
+   res.status(201).json("User created successfully");
 }
