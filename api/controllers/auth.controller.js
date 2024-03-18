51dc0a0ede5eb8d11ba21c91175a65691c379773
Noah William Shaffer <noahwilliamshaffer@gmail.com>
2024-03-18 14:33:43 -0400

    edited signup controller to accept input, imported our user model to auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,7 @@
+import User from "../models/user.model.js";
+
 export const signup = (req, res) => {
     //info we get from browser
-   console.log(req.body);
+   const {username, email, password} = req.body;
+   const newUser = new Userer({username, email, password});
 }
