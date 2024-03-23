5223754f02157d0620f2db511bfbe48a127698c8
Noah William Shaffer <noahwilliamshaffer@gmail.com>
2024-03-23 15:53:13 -0400

    added the email to my schema lol

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,12 +2,10 @@ import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res) => {
-    //info we get from browser
    const {username, email, password} = req.body;
    const hashedPassword = bcryptjs.hashSync(password, 10);
-   const newUser = new User({username, email, password});
-   //this saving takes time so we use await and change function to async
+
+   const newUser = new User({username, email, password: hashedPassword});
    await newUser.save()
-   //201 is for created
    res.status(201).json("User created successfully!");
 }
