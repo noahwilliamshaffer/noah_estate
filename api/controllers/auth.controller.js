de6f57c574724756f469ec9929505f9f648d9e10
Noah William Shaffer <noahwilliamshaffer@gmail.com>
2024-03-23 15:33:39 -0400

    We use hashSync becuase it has an await built in.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,10 @@
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res) => {
     //info we get from browser
    const {username, email, password} = req.body;
+   const hashedPassword = bcryptjs.hashSync(password, 10);
    const newUser = new User({username, email, password});
    //this saving takes time so we use await and change function to async
    await newUser.save()
