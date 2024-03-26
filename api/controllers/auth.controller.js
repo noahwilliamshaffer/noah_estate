d63c6146ad32b873a94d1a888753ed78d1681669
Noah William Shaffer <noahwilliamshaffer@gmail.com>
2024-03-26 14:24:52 -0400

    SignIn Function is successful!

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
    const {username, email, password} = req.body;
@@ -14,4 +15,21 @@ export const signup = async (req, res, next) => {
         next(error);
    }
    
+};
+
+export const signin = async (req, res, next) => {
+     const {email, password} = req.body;
+     try{
+         const validUser = await User.findOne({email}); 
+         if(!validUser) return Usernext(errorHandler(404, "User not found!")); 
+         const validPassword = bcryptjs.compareSync(password, validUser.password);
+         if(!validPassword) return next(errorHandler(401, "Invalid password!"));
+         const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+         res.cookie('access_token', token, {httpOnly: true})
+         .status(200)
+         .json({validUser});
+     }
+     catch(error){
+          next(error);
+     }
 }
