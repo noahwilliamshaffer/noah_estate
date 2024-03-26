7b09b43228a9e73ce1280ea5e0570bf55e744775
Noah William Shaffer <noahwilliamshaffer@gmail.com>
2024-03-26 14:31:35 -0400

    Removed password from returning to the user for security reason.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -24,10 +24,11 @@ export const signin = async (req, res, next) => {
          if(!validUser) return Usernext(errorHandler(404, "User not found!")); 
          const validPassword = bcryptjs.compareSync(password, validUser.password);
          if(!validPassword) return next(errorHandler(401, "Invalid password!"));
-         const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+         const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+         const { password: Pass, ...rest } = validUser._doc;
          res.cookie('access_token', token, {httpOnly: true})
          .status(200)
-         .json({validUser});
+         .json(rest);
      }
      catch(error){
           next(error);
