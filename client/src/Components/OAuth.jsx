d2f618742cf455af4542d714039cebe0fde3ec02
Noah William Shaffer <noahwilliamshaffer@gmail.com>
2024-03-29 21:19:23 -0400

    Sign in succsessfful.

--- client/src/Components/OAuth.jsx
+++ client/src/Components/OAuth.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { app } from '../firebase'; // Ensure this is the correct path to your Firebase config file
+import { useDispatch } from 'react-redux';
+import { SignInSuccess } from '../redux/user/userSlice';
 
 export default function OAuth() {
+    const dispatch = useDispatch();
+    const handleGoogleClick = async () => {
+        try {
+            const provider = new GoogleAuthProvider();
+            const auth = getAuth(app); // Pass the app instance to getAuth
+            const result = await signInWithPopup(auth, provider);
+            console.log(result);
+            const res = await fetch('/api/auth/google', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify( { name: result.user.displayName, email: result.user.email, photo: result.user.photoURL } ),
+            });
+            const data = await res.json();
+           dispatch(SignInSuccess(data));
+
+        } catch (error) {
+            console.log('could not sign in with google', error);
+        }
+    };
+
     return (
-        <button className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
-            Continue with google
+        <button onClick={handleGoogleClick} type='button' className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
+            Continue with Google
         </button>
-    )
+    );
 }
---
+++ client/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-estate-c1d11.firebaseapp.com",
+  projectId: "mern-estate-c1d11",
+  storageBucket: "mern-estate-c1d11.appspot.com",
+  messagingSenderId: "715936191842",
+  appId: "1:715936191842:web:e343898fd115cd0adc483b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export { app };
