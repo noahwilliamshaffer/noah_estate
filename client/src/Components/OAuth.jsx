5860c89dd1f99b161057fb7dacbfb2fbd63163b7
Noah William Shaffer <noahwilliamshaffer@gmail.com>
2024-03-30 09:08:28 -0400

    After google sign in the app navigates user to homepage.

--- client/src/Components/OAuth.jsx
+++ client/src/Components/OAuth.jsx
@@ -2,9 +2,11 @@ import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import { app } from '../firebase'; // Ensure this is the correct path to your Firebase config file
 import { useDispatch } from 'react-redux';
 import { SignInSuccess } from '../redux/user/userSlice';
+import { useNavigate } from 'react-router-dom';
 
 export default function OAuth() {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const handleGoogleClick = async () => {
         try {
             const provider = new GoogleAuthProvider();
@@ -20,6 +22,8 @@ export default function OAuth() {
             });
             const data = await res.json();
            dispatch(SignInSuccess(data));
+           navigate('/');
+
 
         } catch (error) {
             console.log('could not sign in with google', error);
