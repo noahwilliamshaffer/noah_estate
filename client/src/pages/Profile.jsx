61060663b8bc5f6c53dcbfd78bd90361579424b0
Noah William Shaffer <noahwilliamshaffer@gmail.com>
2024-04-02 08:21:12 -0400

    Upload function executes correctly, logs the percentages while uploading.

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { get } from 'mongoose';
+import { app } from '../firebase';
 // Ensure Firebase is initialized outside this component
 
 export default function Profile() {
@@ -11,7 +13,25 @@ export default function Profile() {
   console.log(file);
 
 
- 
+  useEffect(() => {
+    if(file){ 
+      handleFileUpload();
+    }
+  }, [file]);
+
+  const handleFileUpload = () => {
+    const storage = getStorage(app);
+    const fileName =  new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed',  
+    (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+    },
+    
+    )};
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
