0cae01d7f3b98821047c0d1310a4d62a13afd4c1
Noah William Shaffer <noahwilliamshaffer@gmail.com>
2024-04-04 08:12:38 -0400

    Managed the error handling

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
 
 
 export const test = (req, res) => {
@@ -8,12 +9,10 @@ export const test = (req, res) => {
     });
 }
 
-export const updateUser = async (req, res) => {
-   if(req.user.id !== req.params.id) {
-       return res.status(401).json({
-           message: 'You can only update your own account!'
-       });
-   }
+export const updateUser = async (req, res, next) => {
+   if(req.user.id !== req.params.id) 
+       return next(errorHandler(401,'You can only update your own account!'));
+    
    try {
         if(req.body.password) {
             req.body.password = bcryptjs.hashSync(req.body.password, 8);
