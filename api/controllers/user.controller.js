50de835ce0f65cffcd60341ac11d444257efb01d
Noah William Shaffer <noahwilliamshaffer@gmail.com>
2024-04-14 13:57:30 -0400

    Create a user list associated with id.

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -50,18 +50,6 @@ export const deleteUser = async (req, res, next) => {
   }
 };
 
-export const getUserListings = async (req, res, next) => {
-  if (req.user.id === req.params.id) {
-    try {
-      const listings = await Listing.find({ userRef: req.params.id });
-      res.status(200).json(listings);
-    } catch (error) {
-      next(error);
-    }
-  } else {
-    return next(errorHandler(401, 'You can only view your own listings!'));
-  }
-};
 
 export const getUser = async (req, res, next) => {
   try {
@@ -78,3 +66,15 @@ export const getUser = async (req, res, next) => {
   }
 };
 
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, 'You can only view your own listings!'));
+  }
+};
