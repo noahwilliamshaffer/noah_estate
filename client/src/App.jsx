a16610268dbf446d55581e089d04a24bab7c5158
Noah William Shaffer <noahwilliamshaffer@gmail.com>
2024-04-20 16:09:44 -0400

    I had to clear vs studios local cache to resolve the bug,  I think it works now.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import PrivateRoute from './components/PrivateRoute'; // Ensure PrivateRoute is 
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
+import Search from './pages/Search';
 
 export default function App() {
     return (
@@ -21,6 +22,7 @@ export default function App() {
                 <Route path="/about" element={<About />} />
                 <Route path="/signin" element={<SignIn />} />
                 <Route path='/listing/:listingId' element={<Listing />} />
+                <Route path="/search" element={<Search />} />
 
         
                 <Route element = {<PrivateRoute />} >  
