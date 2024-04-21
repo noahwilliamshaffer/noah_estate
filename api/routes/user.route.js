41776b72ead2a453d78f32a3d1e3bcca37616a33
Noah William Shaffer <noahwilliamshaffer@gmail.com>
2024-04-21 14:44:11 -0400

    Reimplemented partial user.route file.

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import { test, updateUser, deleteUser } from '../controllers/user.controller.js';
+//import { deleteUser, test, updateUser,  getUserListings, getUser} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
 const router = express.Router();
 
 router.get('/test', test);
-router.post('/update/:id', verifyToken, updateUser); 
-router.delete('/delete/:id', verifyToken, deleteUser);
-
-//create token inside the cookie to authenticate the user
+router.post('/update/:id', verifyToken, updateUser)
+router.delete('/delete/:id', verifyToken, deleteUser)
+//router.get('/listings/:id', verifyToken, getUserListings) 
+//router.get('/:id', verifyToken, getUser)
 
 
 export default router;
