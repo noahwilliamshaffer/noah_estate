48cdc3a0f6cae0c3b1a699f44f441611feaa5da7
Noah William Shaffer <noahwilliamshaffer@gmail.com>
2024-04-21 14:51:17 -0400

    Added google login to signup page.

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,69 +1,85 @@
-import { set } from 'mongoose';
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom'; // Import Link from react-router-dom if you're using it for routing
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import OAuth from '../components/OAuth';
 
 export default function SignUp() {
-  const [formData, setFormData] = useState({}); // Initialize form data state
-  const [error, setError] = useState(null); // Initialize error state
-  const [loading, setLoading] = useState(false); // Initialize loading state
-  const navigate = useNavigate(); // Initialize navigate function from useNavigate hook
-
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   const handleChange = (e) => {
-    setFormData({ 
-      ...formData, 
+    setFormData({
+      ...formData,
       [e.target.id]: e.target.value,
-    }); // Function to update form data on input change
-   // console.log(formData); // Log the updated form data
+    });
   };
-
   const handleSubmit = async (e) => {
-    e.preventDefault(); // Prevent form from refreshing the page
-    try{
-    setLoading(true); // Set loading state to true
-    const res = await fetch('/api/auth/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    }); // Send a POST request to the server with the form data
-    const data = await res.json();
-
-    if (data.success === false) {
-      setError(data.message); // 
-      setLoading(false); 
-      return;
-    }
-    setLoading(false); 
-    setError(null);
-    navigate('/SignIn');
-  } catch (error) {
-    setError(error.message);
-    setLoading(false);
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/sign-in');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
     }
   };
-  console.log(formData); // Log the response from the server
-
-
   return (
-    <div className='p-3 max-w-lg'>
-      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form onSubmit={handleSubmit} className='flex flex-col gap-4'> 
-        <input type="text" placeholder="Username" className="block border p-3 rounded-lg" id="username" onChange={handleChange} />
-        <input type="email" placeholder="Email" className="block border p-3 rounded-lg" id="email" onChange={handleChange} />
-        <input type="password" placeholder="Password" className="block border p-3 rounded-lg" id="password" onChange={handleChange} />
-        <button disabled = {loading} type="submit" className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 
-        disabled:opacity-80'>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          type='text'
+          placeholder='username'
+          className='border p-3 rounded-lg'
+          id='username'
+          onChange={handleChange}
+        />
+        <input
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+          onChange={handleChange}
+        />
+
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
           {loading ? 'Loading...' : 'Sign Up'}
-           </button>
+        </button>
+        <OAuth/>
       </form>
-      <div className="flex gap-2 mt-5">
+      <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
-        <Link to='/SignIn'>
-          <span className="text-blue-700">Sign In</span>
+        <Link to={'/SignIn'}>
+          <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
-      {error && <p className="text-red-500 mt-3">{error}</p>} 
-    </div> // Display error message if there's an error from try catch block in handleSubmit function
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
+    </div>
   );
 }
