d2dc250bc994211b66712e7e728cd051c77e2091
Noah William Shaffer <67817365+noahwilliamshaffer@users.noreply.github.com>
2024-08-24 13:09:04 -0400

    Updated the readme.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,8 +10,8 @@ function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
         <Link to='/'> 
-          <span className='text-slate-500'>Shaffer</span>
-          <span className='text-slate-700'>Estate</span>
+          <span className='text-slate-500'>Two Broke</span>
+          <span className='text-slate-700'>Brothers</span>
           </Link>
         </h1>
         <form className='bg-slate-100 rounded-lg flex items-center'>
